6147c3a90b83d692430b131b7aae8291f2a11374
smilehot21 <sergeybusenko@gmail.com>
2016-11-03 01:51:30 +0200

    eclipse project

---
+++ lt_tests/src/com/example/tests/Untitled.java
@@ -0,0 +1,80 @@
+package com.example.tests;
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class Untitled {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+
+  @Before
+  public void setUp() throws Exception {
+	  System.setProperty("webdriver.gecko.driver", "C:\\Users\\bort747\\Projects\\lt_tests\\geckodriver.exe");
+	  driver = new FirefoxDriver();
+    
+    baseUrl = "http://172.16.30.101:9008/";
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+  @Test
+  public void testUntitled() throws Exception {
+    driver.get(baseUrl + "login");
+    driver.findElement(By.name("email")).clear();
+    driver.findElement(By.name("email")).sendKeys("busenkosergey@gmail.com");
+    driver.findElement(By.name("password")).clear();
+    driver.findElement(By.name("password")).sendKeys("Qwe123");
+    driver.findElement(By.xpath("//button[@type='button']")).click();
+    driver.findElement(By.cssSelector("span.ng-binding")).click();
+    driver.findElement(By.id("headerLogoutLink")).click();
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
